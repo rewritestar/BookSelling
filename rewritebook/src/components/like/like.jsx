2452727dffa6e4b6bfa4ee82b73ed925d6067abc
rewritestar <rewritestar@naver.com>
2022-09-27 19:46:27 +0900

    like 수정-readme수정

--- rewritebook/src/components/like/like.jsx
+++ rewritebook/src/components/like/like.jsx
@@ -20,20 +20,22 @@ const Like = ({ HeaderCartCount, likeService }) => {
       <div className={styles.title}>
         <Title name="Like" />
       </div>
-      <section className={styles.like_container}>
-        {books.map((book, i) => {
-          let pink = 0;
-          if (i % 2) pink = 1;
-          return (
-            <LikeItem
-              key={book.id}
-              book={book}
-              handleDelete={handleDelete}
-              pink={pink}
-            />
-          );
-        })}
-      </section>
+      <div className={styles.content}>
+        <section className={styles.like_container}>
+          {books.map((book, i) => {
+            let pink = 0;
+            if (i % 2) pink = 1;
+            return (
+              <LikeItem
+                key={book.id}
+                book={book}
+                handleDelete={handleDelete}
+                pink={pink}
+              />
+            );
+          })}
+        </section>
+      </div>
       <Footer />
     </div>
   );
